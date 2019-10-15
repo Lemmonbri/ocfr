5d4afb1f45214185e33abe61aeea9d62eea15be6
Brian Emmons <lemmonbri@gmail.com>
2019-10-15 17:23:39 -0400

    Added sql files

---
+++ data/certifications.sql
@@ -0,0 +1,15 @@
+truncate certifications;
+
+INSERT INTO certifications (cName, cAgency, expPeriod) VALUES
+("CPR","CPR for Healthcare Providers/American Heart Association","3"),
+("CPR","CPR for the Professional Rescuer/American Red Cross","2"),
+("Firefighter I","Athens Technical College","1"),
+("Firefighter I","Ivy Technical College","4"),
+("Firefighter II","Ivy Technical College","2"),
+("POST","Georgia POST Academy","4"),
+("HAZMAT","Athens Technical College","3"),
+("Extrication","Georgia Fire College",""),
+("EMT-Adv","Georgia Department of Homeland Security","2"),
+("Due Regard","Athens Technical College","3"),
+("Paramedic","Watkinsville Technical College","1")
+;
---
+++ data/createTable.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE ocfr;
+USE ocfr;
+
+CREATE TABLE people(
+	pID int PRIMARY KEY AUTO_INCREMENT,
+    firstName varchar(64) NOT NULL,
+    lastName varchar(64) NOT NULL,
+    dob DATE NOT NULL,
+    gender ENUM('M', 'F') NOT NULL,
+    email varchar(64),
+    wPhone varchar(25),
+    mPhone varchar(25),
+    address varchar(100),
+    city varchar(64),
+    state varchar(2),
+    zip varchar(10),
+    startDate DATE NOT NULL,
+    jobTitle varchar(64),
+    radioNumber varchar(10),
+    stationNumber varchar(10),
+    isActive boolean NOT NULL
+);
+
+CREATE TABLE certifications(
+	cID int PRIMARY KEY AUTO_INCREMENT,
+	cName varchar(100),
+    cAgency varchar(100),
+    expPeriod int
+);
+
+CREATE TABLE people_certifications(
+	pID int,
+	cID int,
+    PRIMARY KEY (pID, cID),
+	FOREIGN KEY (pID) REFERENCES people(pID),
+    FOREIGN KEY (cID) REFERENCES certifications(cID)
+);
